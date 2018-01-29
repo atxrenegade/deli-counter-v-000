2f7895d63274c7c0cf067889ab4a0e9cca4ae36e
Harleigh Abel <harleighabel@gmail.com>
2018-01-29 21:18:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,9 +12,8 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli << name
-  number = katz_deli.count + 1
-  puts "Welcome, #{name}. You are number #{number} in line."
+  katz_deli << name 
+  puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
 end
 
 def now_serving(katz_deli)
