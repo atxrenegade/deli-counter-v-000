a5f821a9ce08ca167a8f8ffc9873cf5e91ab42bb
Harleigh Abel <harleighabel@gmail.com>
2018-01-29 21:01:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
   else
     print "The line is currently: "
     katz_deli.each_with_index do |name, index|
-      print "#{index + 1}. #{name} "
+      print "#{index + 1}. #{name}"
     end
   end
 end
