c675ee149aac55c382369423150000ba952d8da6
Harleigh Abel <harleighabel@gmail.com>
2018-01-29 21:00:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,8 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     print "The line is currently "
-    katz_deli.each_with_index do |name|
-      puts "#{name.index + 1} #{name}"
+    katz_deli.each_with_index do |name, index|
+      print "#{index + 1}. #{name} "
     end
   end
 end
